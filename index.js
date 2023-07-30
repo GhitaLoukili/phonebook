e9ef650c136b3e087467f1dc55eab62f71b2d35a
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2023-07-30 23:42:06 +0100

    3.11

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ morgan.token('type', (req, res) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(express.static('build'))
 
 let persons = [
     { 
